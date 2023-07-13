ca5c7cdf85c2c8f0f9a049840111b340f8114a83
Elmahdi Ratil <elmahdiratildev@gmail.com>
2023-07-13 16:08:03 -0700

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print a string without using printf only with puts
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingualpuzzle");
+	return (0);
+}
