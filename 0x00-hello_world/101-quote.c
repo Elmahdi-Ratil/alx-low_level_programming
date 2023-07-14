d3061710d7a2b789df2ed7f83a7e24681c56050a
Elmahdi Ratil <elmahdiratildev@gmail.com>
2023-07-13 17:49:58 -0700

    Intel quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,16 +2,15 @@
  * File: 101-quote.c
  * Auth: MahDev
  */
+#include <stdio.h>
 #include <unsistd.h>
 /**
- * main - Entry point
+ * main - A c program that prints a line to the standard error
  *
- *
- * Return: Always 1 (Failure)
+ * Return: 1 (Success)
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n",59);
-
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
