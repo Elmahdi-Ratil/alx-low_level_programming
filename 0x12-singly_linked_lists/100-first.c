b996f1d94f5bdddd5605663ed888843be67729c6
Elmahdi Ratil <elmahdiratildev@gmail.com>
2023-09-21 20:01:30 -0700

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+void before_main(void) __attribute__((constructor));
+
+/**
+ * before_main - This function is executed before main.
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
