a5c751c0f3bdbd599b2794fe09528086c458cd25
Elmahdi Ratil <elmahdiratildev@gmail.com>
2023-09-26 09:29:56 -0700

    add free listint2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ *free_listint2 - free list and header
+ *@head: head of linked list
+ *
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *next;
+
+	if (head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
+	*head = NULL;
+}
